a960e6af760f77741a6cd99c464c26647bc82834
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-25 17:36:42 -0300

    building test

---
+++ src/test/java/org/example/demo/catalinManagingData/SimpleTransitionsTest.java
@@ -0,0 +1,41 @@
+package org.example.demo.catalinManagingData;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.example.demo.models.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleTransitionsTest {
+
+	private static EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("ch10");
+
+    private static EntityManagerFactory emf2 =
+            Persistence.createEntityManagerFactory("ch10_replicate");
+
+    @Test
+    public void makePersistent() {
+        EntityManager em = emf.createEntityManager(); // Application-managed
+
+        em.getTransaction().begin();
+        Item item = new Item();
+        item.setName("Some Item"); // Item#name is NOT NULL!
+        em.persist(item);
+
+        Long ITEM_ID = item.getId();
+
+        em.getTransaction().commit();
+        em.close();
+
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+        assertEquals("Some Item", em.find(Item.class, ITEM_ID).getName());
+        em.getTransaction().commit();
+        em.close();
+
+    }
+}
