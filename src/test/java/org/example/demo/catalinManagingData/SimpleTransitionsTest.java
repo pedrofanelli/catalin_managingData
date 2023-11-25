5c5714db65682382ee29fb5831aaae2699434ca5
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-25 18:01:26 -0300

    changes

--- src/test/java/org/example/demo/catalinManagingData/SimpleTransitionsTest.java
+++ src/test/java/org/example/demo/catalinManagingData/SimpleTransitionsTest.java
@@ -21,7 +21,7 @@ public class SimpleTransitionsTest {
 
         em.getTransaction().begin();
         Item item = new Item();
-        item.setName("Some Item"); // Item#name is NOT NULL!
+        item.setName("Some Item qwer"); // Item#name is NOT NULL!
         em.persist(item);
 
         Long ITEM_ID = item.getId();
@@ -31,9 +31,53 @@ public class SimpleTransitionsTest {
 
         em = emf.createEntityManager();
         em.getTransaction().begin();
-        assertEquals("Some Item", em.find(Item.class, ITEM_ID).getName());
+        assertEquals("Some Item qwer", em.find(Item.class, ITEM_ID).getName());
         em.getTransaction().commit();
         em.close();
 
+    }
+    
+    @Test
+    public void retrievePersistent() throws Exception {
+        EntityManager em = emf.createEntityManager(); // Application-managed
+        em.getTransaction().begin();
+        Item someItem = new Item();
+        someItem.setName("Some Item");
+        em.persist(someItem);
+        em.getTransaction().commit();
+        em.close();
+        Long ITEM_ID = someItem.getId();
+
+        {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            // Hit the database if not already in persistence context
+            Item item = em.find(Item.class, ITEM_ID);
+            if (item != null)
+                item.setName("New Name");
+            em.getTransaction().commit(); // Flush: Dirty check and SQL UPDATE
+            em.close();
+        }
+
+        {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            Item itemA = em.find(Item.class, ITEM_ID);
+            Item itemB = em.find(Item.class, ITEM_ID); // Repeatable read
+
+            assertTrue(itemA == itemB);
+            assertTrue(itemA.equals(itemB));
+            assertTrue(itemA.getId().equals(itemB.getId()));
+
+            em.getTransaction().commit(); // Flush: Dirty check and SQL UPDATE
+            em.close();
+        }
+
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+        assertEquals("New Name", em.find(Item.class, ITEM_ID).getName());
+        em.getTransaction().commit();
+        em.close();
     }
 }
